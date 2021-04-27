79fd056a4e17c3670c69d669624a7af2047b65e5
touch-bell <dhkrwhd1!>
2021-04-28 03:19:19 +0900

    spring data jpa 추가 / dev properties 분리 / dev-mysql 셋팅 / 프론트, 디비 연결 테스트 확인

--- src/main/java/com/hs2b/backend/Repository/ListsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hs2b.backend.Repository;
-
-import com.hs2b.backend.Entity.ListTest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ListsRepository extends JpaRepository<ListTest, Integer> {
-}
