8e0d1d2f133d1f7e1fd8f47c4cfe1c6f87a1f9eb
touch-bell <dhkrwhd1!>
2021-05-01 12:08:09 +0900

    code deploy change

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ REPOSITORY=/home/ec2-user/app/hs2b
 PROJECT_NAME=hs2b-backend
 
 echo "> Build 파일 복사"
-cp $REPOSITORY/backend/*jar $REPOSITORY/
+cp $REPOSITORY/backend/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 에플리케이션 pid 확인"
 
