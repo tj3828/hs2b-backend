d3f6d3ad66985d38d2f14280e3de1608ee5790fd
touch-bell <dhkrwhd1!>
2021-05-01 17:32:28 +0900

    무중단 배포 적용

---
+++ scripts/health.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+ABSPATH=$(readline -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSPATH}/profile.sh
+source ${ABSDIR}/switch.sh
+
+IDLE_PORT=$(find_idle_port)
+IDLE_PROFILE=$(find_idle_profile)
+
+echo "> Health 체크 시작"
+echo "> IDLE_PORT: $IDLE_PORT"
+echo "> curl -s http://localhost:$IDLE_PORT/api/profile"
+sleep 10
+
+for RETRY_COUNT in {1..10}
+do
+  RESPONSE=$(curl -s https://localhost:$IDLE_PORT/api/profile)
+  UP_COUNT=$(echo ${RESPONSE} | grep ${IDLE_PROFILE})
+
+  if [ ${UP_COUNT} -ge 1 ]
+  then
+    echo "> Health 체크 성공"
+    switch_proxy
+    break
+  else
+    echo "> Health 체크의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
+    echo "> Health 상태: ${RESPONSE}"
+  fi
+
+  if [ ${RETRY_COUNT} -eq 10 ]
+  then
+    echo "> Health 체크 실패"
+    echo "> nginx에 연결하지 않고 배포를 종료합니다."
+    exit 1
+  fi
+
+  echo "> Health 체크 연결 실패. 재시도..."
+  sleep 10
+done
---
+++ scripts/profile.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+function find_idle_profile() {
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "${http_code}" http://localhost/api/profile)
+
+    if [ ${RESPONSE_CODE} -ge 400 ]
+    then
+      CURRENT_PROFILE=green
+    else
+      CURRENT_PROFILE=$(curl -s http://localhost/api/profile)
+    fi
+
+    if [ ${CURRENT_PROFILE} == blue ]
+    then
+      IDLE_PROFILE=green
+    else
+      IDLE_PROFILE=blue
+    fi
+
+    echo "CURRENT_PROFILE : ${IDLE_PROFILE}"
+}
+
+function find_idle_port() {
+    IDLE_PROFILE=$(find_idle_profile)
+
+    if [ ${IDLE_PROFILE} == blue ]
+    then
+      echo "8081"
+    else
+      echo "8082"
+    fi
+}
